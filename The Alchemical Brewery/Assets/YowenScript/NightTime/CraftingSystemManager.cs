c94885ecbb3aad7684e38e6f8521fa0893755c8e
changyowen <changyowen@gmail.com>
2020-12-12 08:55:33 +0800

    Update final bah

--- The Alchemical Brewery/Assets/YowenScript/NightTime/CraftingSystemManager.cs
+++ The Alchemical Brewery/Assets/YowenScript/NightTime/CraftingSystemManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class CraftingSystemManager : MonoBehaviour
 {
@@ -144,5 +145,8 @@ public class CraftingSystemManager : MonoBehaviour
         }
     }
 
-
+    public void ExitScene()
+    {
+        SceneManager.LoadScene(1);
+    }
 }
--- The Alchemical Brewery/Assets/YowenScript/NightTime/NextDay.cs
+++ The Alchemical Brewery/Assets/YowenScript/NightTime/NextDay.cs
@@ -9,4 +9,9 @@ public class NextDay : MonoBehaviour
     {
         SceneManager.LoadScene(0);
     }
+
+    public void BrewNewPotion()
+    {
+        SceneManager.LoadScene(3);
+    }
 }

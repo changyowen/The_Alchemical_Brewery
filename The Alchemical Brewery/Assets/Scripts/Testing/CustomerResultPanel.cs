858d490afa7c079b85c9ad7157a433795cf65a0a
changyowen <changyowen@gmail.com>
2021-08-27 02:14:46 +0800

    Update

---
+++ The Alchemical Brewery/Assets/Scripts/Testing/CustomerResultPanel.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CustomerResultPanel : MonoBehaviour
+{
+    public Transform customerContent_transform;
+
+    public IEnumerator StartCustomerContent(CustomerLevelStoring[] initialCustomerLevelArray, CustomerLevelStoring[] finalCustomerLevelArray)
+    {
+        ///ACTIVATE EACH CUSTOMER CONTENT
+        for (int i = 0; i < finalCustomerLevelArray.Length; i++)
+        {
+            //set active customer content
+            Transform currentCustomerContent = customerContent_transform.GetChild(i);
+            currentCustomerContent.gameObject.SetActive(true);
+
+            //reference customer content
+            Image customerImage_image = currentCustomerContent.GetChild(0).GetComponent<Image>();
+            Text customerName_text = currentCustomerContent.GetChild(1).GetComponent<Text>();
+            Text customerLevel_text = currentCustomerContent.GetChild(2).GetComponent<Text>();
+            Transform customerExperienceBarPivot = currentCustomerContent.GetChild(3).GetChild(0);
+
+            //assign initial customer level
+            customerImage_image.sprite = initialCustomerLevelArray[i].customerSprite;
+            customerName_text.text = initialCustomerLevelArray[i].customerName;
+            if (initialCustomerLevelArray[i].customerLevel == 6)
+            {
+                customerLevel_text.text = "LV MAX";
+            }
+            else
+            {
+                customerLevel_text.text = "LV " + initialCustomerLevelArray[i].customerLevel;
+            }
+            Vector3 newlocalScale = new Vector3(initialCustomerLevelArray[i].customerExperiencePercentage, 1, 1);
+            customerExperienceBarPivot.localScale = newlocalScale;
+
+            //get initial and final level
+            float initialLevel = 0;
+            if (initialCustomerLevelArray[i].customerLevel != 6)
+            {
+                initialLevel = (float)initialCustomerLevelArray[i].customerLevel + initialCustomerLevelArray[i].customerExperiencePercentage;
+            }
+            else
+            {
+                initialLevel = 6f;
+            }
+
+            float finalLevel = 0f;
+            if (finalCustomerLevelArray[i].customerLevel != 6)
+            {
+                finalLevel = (float)finalCustomerLevelArray[i].customerLevel + finalCustomerLevelArray[i].customerExperiencePercentage;
+            }
+            else
+            {
+                finalLevel = 6f;
+            }
+
+            ///START LEVELING ANIMATION
+            while (initialLevel != finalLevel)
+            {
+                initialLevel += 1 * Time.unscaledDeltaTime;
+
+                if (initialLevel >= finalLevel)
+                {
+                    initialLevel = finalLevel;
+                }
+
+                if (initialLevel >= 6f)
+                {
+                    Vector3 tempVector3 = new Vector3(1, 1, 1);
+                    customerExperienceBarPivot.localScale = tempVector3;
+                }
+                else
+                {
+                    Vector3 tempVector3 = new Vector3(initialLevel % 1, 1, 1);
+                    customerExperienceBarPivot.localScale = tempVector3;
+                }
+
+                if (initialLevel < 6)
+                {
+                    customerLevel_text.text = "LV " + (int)initialLevel;
+                }
+                else
+                {
+                    customerLevel_text.text = "LV MAX";
+                }
+                yield return null;
+            }
+            yield return new WaitForSeconds(0.2f);
+        }
+
+    }
+}
---
+++ The Alchemical Brewery/Assets/Scripts/Testing/ResultManager.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResultManager : MonoBehaviour
+{
+    public static ResultManager Instance { get; private set; }
+
+    public CustomerResultPanel customerResultPanel;
+
+    public GameObject customerResultPanel_obj;
+
+    CustomerLevelStoring[] initialCustomerLevelArray;
+    CustomerLevelStoring[] finalCustomerLevelArray;
+
+    bool showingCustomerContent = false;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void AssignInitialCustomerLevelArray(List<CustomerData> _customerTypeToday)
+    {
+        ///DECLARE CUSTOMER STORING ARRAY 
+        initialCustomerLevelArray = new CustomerLevelStoring[_customerTypeToday.Count];
+
+        ///ASSIGN DATA FOR CUSTOMER STORING ARRAY
+        for (int i = 0; i < initialCustomerLevelArray.Length; i++)
+        {
+            //get corresponding customer profile
+            CustomerProfile currentCustomerProfile = PlayerProfile.customerProfile[_customerTypeToday[i].customerIndex];
+            //assign data
+            CustomerLevelStoring newCustomerLevelStoring = new CustomerLevelStoring();
+            newCustomerLevelStoring.customerName = _customerTypeToday[i].customerName;
+            newCustomerLevelStoring.customerLevel = currentCustomerProfile.customerLevel;
+            if (newCustomerLevelStoring.customerLevel != 6)
+            {
+                newCustomerLevelStoring.customerExperiencePercentage = currentCustomerProfile.customerExperience / (float)_customerTypeToday[i].levelingExperience[currentCustomerProfile.customerLevel - 1];
+            }            
+            //assign into customer storing array
+            initialCustomerLevelArray[i] = newCustomerLevelStoring;
+        }
+    }
+
+    public void AssignFinalCustomerLevelArray(List<CustomerData> _customerTypeToday)
+    {
+        ///DECLARE CUSTOMER STORING ARRAY 
+        finalCustomerLevelArray = new CustomerLevelStoring[_customerTypeToday.Count];
+        ///ASSIGN DATA FOR CUSTOMER STORING ARRAY
+        for (int i = 0; i < initialCustomerLevelArray.Length; i++)
+        {
+            //get corresponding customer profile
+            CustomerProfile currentCustomerProfile = PlayerProfile.customerProfile[_customerTypeToday[i].customerIndex];
+            //assign data
+            CustomerLevelStoring newCustomerLevelStoring = new CustomerLevelStoring();
+            newCustomerLevelStoring.customerName = _customerTypeToday[i].customerName;
+            newCustomerLevelStoring.customerLevel = currentCustomerProfile.customerLevel;
+            if(newCustomerLevelStoring.customerLevel != 6)
+            {
+                newCustomerLevelStoring.customerExperiencePercentage = currentCustomerProfile.customerExperience / (float)_customerTypeToday[i].levelingExperience[currentCustomerProfile.customerLevel - 1];
+            }
+            //assign into customer storing array
+            finalCustomerLevelArray[i] = newCustomerLevelStoring;
+        }
+    }
+
+    public IEnumerator StartResult()
+    {
+        ///SHOW CUSTOMER LEVELING RESULT
+        StartCoroutine(StartCustomerResultPanel());
+
+        //show each unlocked stuff
+        yield return null;
+        
+    }
+
+    public IEnumerator StartCustomerResultPanel()
+    {
+        bool skip = false;
+        ///ACTIVATE CUSTOMER RESULT PANEL
+        customerResultPanel_obj.SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+
+        ///START CUSTOMER CONTENT ANIMATION
+        showingCustomerContent = true;
+        yield return StartCoroutine(customerResultPanel.StartCustomerContent(initialCustomerLevelArray, finalCustomerLevelArray));
+        showingCustomerContent = false;
+
+        ///WAIT TILL PLAYER CLICK
+        while (!skip)
+        {
+            if (Input.GetMouseButtonDown(0))
+                skip = true;
+            else
+                yield return null;
+        }
+        ///DEACTIVATE CUSTOMER RESULT PANEL
+        customerResultPanel_obj.SetActive(false);
+    }
+}
+
+public class CustomerLevelStoring
+{
+    public Sprite customerSprite; //customer sprite
+    public string customerName; //customer name
+    public int customerLevel; //customer current level
+    public float customerExperiencePercentage = 1; //customer current experience bar percentage
+}
